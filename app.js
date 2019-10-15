d313cd0bce17f798bf8924c7fba3a27c2ff2b95f
jooprima <jooprima@gmail.com>
2019-10-15 13:43:55 +0700

    create Express server

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const request = require("request");
+const bodyParser = require("body-parser");
+const pug = require("pug");
+const _ = require("lodash");
+const path = require("path");
+const { Donor } = require("./models/donor");
+const { initializePayment, verifyPayment } = require("./config/paystack")(
+  request
+);
+
+const port = process.env.PORT || 3000;
+const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, "public/")));
+app.set("view engine", pug);
